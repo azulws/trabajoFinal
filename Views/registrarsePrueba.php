87a7ac0eab5e0a971deffc267be5f81bd9ded875
Antonela <antonelalucaibertarini@gmail.com>
2019-10-27 17:16:40 -0300

    prueba

---
+++ Views/registrarsePrueba.php
@@ -0,0 +1,6 @@
+ 
+ <form method="POST" style="background-image:url('../Views/img/fondo1.jpg');padding: 2rem !important;" action=<?php echo FRONT_ROOT."Login/createUserDB";?>>
+
+ 	<button class="submit" name="registrarse">registrarse</button>
+
+ </form> 
