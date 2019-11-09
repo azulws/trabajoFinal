e2f63e51887c04d5ab7530c5b2e2eccb339ee593
Antonela <antonelalucaibertarini@gmail.com>
2019-11-09 18:23:40 -0300

    cambios en el nav y register

--- trabajoFinal/Views/navLog.php
+++ trabajoFinal/Views/navLog.php
@@ -1,23 +1,23 @@
 <div id= "nav">
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary d-flex justify-content-end">
      <p> <?php $message ?> </p>
-     <form id="form-nav-log" method="POST" action=<?php echo FRONT_ROOT.'Login/log';?>>
+     <form class="form-nav-log" method="POST" action=<?php echo FRONT_ROOT.'Login/log';?>>
           <ul class="nav justify-content-center ml-auto">
-               <li class="nav-item" id="nav-item" >
-                    <input id="inputs" class ="type="email" placeholder="Email" name="user_email" required>
+               <li class="nav-item"  >
+                    <input class="inputs" class ="type="email" placeholder="Email" name="user_email" required>
                </li>
-               <li class="nav-item" id="nav-item">
-                    <input id="inputs" type="password" placeholder="Password" name="password" required>
+               <li class="nav-item" >
+                    <input class="inputs" type="password" placeholder="Password" name="password" required>
                </li>
-               <li class="nav-item"id="nav-item" >
-                    <button id="buttons" class="btn btn-danger float-right" type="submit">Ingresar</a>
+               <li class="nav-item" >
+                    <button class=" buttons btn btn-danger float-right" type="submit">Ingresar</a>
                </li>
           </ul>
      </form>
-     <form id="form-nav-log" action=<?php echo FRONT_ROOT.'Login/register'?>>
+     <form class="form-nav-log" action=<?php echo FRONT_ROOT.'Login/register'?>>
         <ul class="nav justify-content-center ml-auto">
-              <li class="nav-item"id="nav-item" >
-                   <button id="buttons" class="btn btn-danger float-right" type="submit">Registrarse</button>
+              <li class="nav-item" >
+                   <button class=" buttons btn btn-danger float-right" type="submit">Registrarse</button>
               </li>
        </ul>
      </form>
--- trabajoFinal/Views/registrarse.php
+++ trabajoFinal/Views/registrarse.php
@@ -1,21 +1,40 @@
-<form method="POST" style="background-image:url('../Views/img/fondo1.jpg');padding: 2rem !important;" action=<?php echo FRONT_ROOT."Login/createUserDB";?>>
-		<div align="center">
-     		<h2>Registro de usuario</h2>
-             <input type="text" name="name" placeholder="Name" required>
-			<br>
-            <input type="text" name="lastname" placeholder="Lastname" required>
-            <br>
-             <input type="email" name="email" placeholder="Email" required>
-     		<br>
-             <input type="password" name="password" placeholder="Contraseña" required>
-            <br>
-            <input type="number" name="dni" placeholder="dni" required min=1000000>
-            <br>
-            <button type="submit" name="register">Registrarse</button>
-         </div>
+
+   <div class="container">        
+   <div class="row justify-content-center"> 
+   <form class="form-register " method="POST" action="<?php echo FRONT_ROOT.'Login/createUserDB';?>">
+    
+     <h2 class="form-register-title">Registro de usuario</h2>
+
+     <ul class="form-register-ul bg-primary"> 
+        <li class="form-register-li">
+            <input class="form-register-input" type="text" name="name" placeholder="Name" required>
+        </li>
+        <li class="form-register-li">
+            <input class="form-register-input" type="text" name="lastname" placeholder="Lastname" required>
+        </li>
+        <li class="form-register-li">
+             <input class="form-register-input" type="email" name="email" placeholder="Email" required>
+        </li>
+         <li class="form-register-li">            
+             <input class="form-register-input" type="password" name="password" placeholder="Contraseña" required>
+        </li>
+         <li class="form-register-li">
+            <input class="form-register-input" type="number" name="dni" placeholder="dni" required min=1000000>
+        </li>
+        <li class="form-register-li">
+            <button class="form-register-buttons btn btn-danger" type="submit" name="register">Registrarse</button>
+        </li>
+        </ul>
+        </form> 
+    </div>
+    <div class="row justify-content-center">
+    <form class="form-register" action="<?php echo FRONT_ROOT.'Login/Index';?>" method='POST'>
+     <ul class="form-register-ul">
+         <li class="form-register-li">
+             <button class="form-register-buttons btn btn-danger" type='submit' name='action'>Volver</button>
+         </li>
+     </ul>
     </form>
-<?php
-echo "<form class='login' action='".FRONT_ROOT."Login/Index' method='POST'>
-        <button type='submit' name='action'>Volver</button>
-    </form>";
-?>
+    </div>
+   </div>
+
